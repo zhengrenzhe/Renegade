ba5860f4a084bc9483a6d2c0f4ed99213fd5460b
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2018-11-11 01:23:12 +0800

    upload default service

--- handler/receive.py
+++ handler/receive.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import io
-
 from telegram.bot import Bot, Update
 from telegram.ext import MessageHandler, Filters
 
-from upload.upyun import upload
+from upload import upload
 from utils import services_buttons
 from middleware.db import db, keys
 from template import choice_service_tpl
@@ -31,22 +29,10 @@ def receive_callback(bot: Bot, update: Update, user_data):
                          reply_to_message_id=update.message.message_id,
                          reply_markup=buttons)
 
-    # file = bot.get_file(file_target.file_id)
-    # raw_file_name = file_target.file_name
-    # file_name = file.file_path.split("/")[-1]
-    # file_binary = file.download_as_bytearray()
+        return
 
-    # f = io.BytesIO(file_binary)
-
-    # if not default_service:
-    #     user_data[]
-    #     bot.send_document(chat_id=update.message.chat_id, document=f)
-    # else:
-    #     pass
-
-    # upload(file_binary, file_name)
-    # bot.send_message(chat_id=update.message.chat_id,
-    #                  text="%s upload success 😜" % file_name)
+    upload(bot, update.message.chat_id, default_service,
+           file_target, update.message.message_id)
 
 
 receive = MessageHandler(Filters.document | Filters.photo,
